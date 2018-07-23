b348a683258d637e57f7a6f0502d75ac99e2ab34
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-23 08:21:46 -0600

    Added notes and throws; added draft of function

--- factorial.php
+++ factorial.php
@@ -1,7 +1,15 @@
 <?php
 /**
+ * Function for calculating the factorial of n
  *
- *
- *
- *
+ * @throws \RangeException if data values are out of bounds (negative integers: if n < 0)
+ * @throws \RangeException if data values are out of bounds (if n > 20, 21! and above overflow the int type)
  */
+
+
+function factorial( $n ) {
+   if ( $n == 0 ) return 1;
+   return factorial( $n-1 ) * $n;
+}
+
+
