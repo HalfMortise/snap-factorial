13dd866152c5336eabe3a72a0c401eb0e9167d96
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-23 08:49:01 -0600

    Next draft

--- factorial.php
+++ factorial.php
@@ -3,13 +3,21 @@
  * Function for calculating the factorial of n
  *
  * @throws \RangeException if data values are out of bounds (negative integers: if n < 0)
- * @throws \RangeException if data values are out of bounds (if n > 20, 21! and above overflow the int type)
+ * @throws \RangeException if data values are out of bounds (if n > 20: 21! and above overflow the int type)
  */
 
 
-function factorial( $n ) {
-   if ( $n == 0 ) return 1;
-   return factorial( $n-1 ) * $n;
+function factorial($in) {
+
+   // 0! = 0! = 1
+   $calculation = 0;
+
+   // Only if $in is >= 0
+   for ($i = 0; $i <= $in; $i++) {
+      $calculation *= $i;
+   } catch(\Exception $exception) {
+      //if the row couldn't be converted, rethrow it
+      throw(new \PDOException($exception->getMessage("Integer can only be between 0-20"), 0, $exception));
+   }
+   return $calculation;
 }
-
-
